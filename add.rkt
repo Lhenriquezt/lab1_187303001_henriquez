7b8f8700467736be4a49f49378c18d0d3a935ac7
Luis Henriquez <luis.henriquezt@usach.cl>
2020-06-01 13:01:18 -0400

    arreglo errores

--- add.rkt
+++ add.rkt
@@ -5,11 +5,11 @@
 ;dom: lista X Zona X lista 
 ;rec: Zona
 ;recurcion : cola
-(define addInterno (lambda (cambios) (lambda(zonas) (lambda(aux)
+(define add (lambda (cambios) (lambda(zonas) 
      (if (null? (index zonas))
          (listazonas(list
               (workspace zonas)
-              (append aux cambios)
+               cambios
               (localRepository zonas)
               (remoteRepository zonas)))
          (if (null? cambios)
@@ -21,14 +21,14 @@
 
              (listazonas(list
               (workspace zonas)
-              (cons (car (index zonas)) (cons (((add cambios))(cdr(index zonas)) aux) null))
+              (append (index zonas) cambios)
               (localRepository zonas)
               (remoteRepository zonas)))
-             ))))))
+             )))))
 ;desc: envia los archivos especificados al Index
 ;dom: lista X Zona
 ;rec:  Zona
-(define add (lambda (cambios) (lambda(zonas) (((addInterno cambios)zonas)null))))
+;(define add (lambda (cambios) (lambda(zonas) (((addInterno cambios)zonas)null))))
 
 
 (define inWorkspace (lambda(cambios zonas)
