754767f90456489f5ba61c9052b9f8fc48ab85c0
luis henriquez <luis.henriquezt@usach.cl>
2020-08-03 23:17:37 -0400

    Modificadores

--- PROLOG/Lab 2- prolog.pl
+++ PROLOG/Lab 2- prolog.pl
@@ -36,3 +36,35 @@ repo_getLocal([Date, Name, Workspace, Index, Local, Remote], Local):-
 %RemoteRepository
 repo_getRemote([Date, Name, Workspace, Index, Local, Remote], Remote):-
        is_repo([Date, Name, Workspace, Index, Local, Remote]).
+
+
+%MODIFICADORES
+%Nombre
+repo_setName([Date, Name, Workspace, Index, LocalRepository, RemoteRepository], NewName, [Date, NewName, Workspace, Index, LocalRepository, RemoteRepository]):-
+     is_repo([Date, Name, Workspace, Index, LocalRepository, RemoteRepository]),
+     string(NewName).
+
+%Workspace
+repo_setWorkspace([Date, Name,  Workspace,Index, LocalRepository, RemoteRepository], NewWorkspace,[Date, Name, NewWorkspace, Index, LocalRepository, RemoteRepository]):-
+          is_repo([Date, Name, Workspace, Index, LocalRepository, RemoteRepository]),
+          is_list(NewWorkspace),
+          is_stringList(NewWorkspace).
+
+%Index
+repo_setIndex([Date, Name, Workspace,Index,LocalRepository,RemoteRepository], NewIndex,[Date, Name, Workspace, NewIndex, LocalRepository, RemoteRepository]):-
+      is_repo([Date, Name, Workspace, Index, LocalRepository, RemoteRepository]),
+      is_list(NewIndex),
+      is_stringList(NewIndex).
+
+%LocalRepository
+repo_setLocal([Date, Name, Workspace, Index, LocalRepository, RemoteRepository], NewLocalRepository,[Date, Name, Workspace, Index, NewLocalRepository, RemoteRepository]):-
+      is_repo([Date, Name, Workspace, Index, LocalRepository, RemoteRepository]),
+      is_list(NewLocalRepository),
+      is_stringList(NewLocalRepository).
+
+%RemoteRepository
+repo_setRemote([Date, Name, Workspace, Index, LocalRepository, RemoteRepository], NewRemoteRepository,[Date, Name, Workspace, Index, LocalRepository, NewRemoteRepository]):-
+       is_repo([Date, Name, Workspace, Index, LocalRepository, RemoteRepository]),
+       is_list(NewRemoteRepository),
+       is_stringList(NewRemoteRepository).
+
